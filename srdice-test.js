39da07d68005801ebb39af1299fcda824d18a86f
Oliver Schrenk <oliver.schrenk@gmx.net>
2010-01-23 00:25:12 +0100

    Rolling success test for Shadowrun in iPhone

--- srdice-test.js
+++
@@ -1,6 +0,0 @@
-function test() {
-	console.log("successTest: ", successTest(1,4)); 
-	console.log("successContest: ", successContest(5, 4, 4, 5)); 
-	console.log("openTest: ", openTest(5)); 
-	console.log("opposedTest: ", opposedTest(5,5)); 
-}
--- srdice.js
+++ srdice.js
@@ -1,209 +1,72 @@
+const SIDES = 6;
+
 function successTest (numberOfDice, targetNumber) {
-	var diceCup = new DiceCup(6);
-	var set = diceCup.cumulativeRoll(numberOfDice, 6);
+	var diceCup = new DiceCup(SIDES);
+	var roll = diceCup.roll(numberOfDice, SIDES);
 
-	if (set.size() == set.count(1)) {
-		throw new RuleOfOneException("CHARACTER");
+	var result = new Object();
+	result.all = roll;
+	result.max = roll[roll.length-1];
+	var myFilter = new GreaterOrEqualTargetNumberFilter(targetNumber);
+	result.successes = roll.filter(myFilter.apply)
+
+	if (roll.length == count(roll, 1)) {
+		result.fail = true;
 	}
 
-	return set.asArray().filter( (new GreaterOrEqualTargetNumberFilter(10).apply)).length;
-}
-
-function successContest (characterRating, characterDefense, opponentRating, opponentDefense) {
-	
-	var characterSucesses = 0;
-	var opponentSuccesses = 0;
-	
-	try {
-		characterSucesses = successTest(characterRating, opponentDefense);
-	} catch (e) {
-		throw new RuleOfOneException("CHARACTER");
-	}
-	
-	try {
-		opponentSuccesses = successTest(opponentRating, characterDefense);
-	} catch (e) {
-		throw new RuleOfOneException("OPPONENT");
-	}
-	
-	return characterSucesses - opponentSuccesses;
-}
-
-function openTest (numberOfDice) {
-	var diceCup = new DiceCup(6);
-	var set = diceCup.cumulativeRoll(numberOfDice, 6);
-	
-	return Array.max(set.asArray());
-}
-
-function opposedTest(valueOfCharacter, valueOfOpponent) {
-	var characterSucesses = 0;
-	var opponentSuccesses = 0;
-	
-	try {
-		characterSucesses = successTest(valueOfCharacter, valueOfOpponent);
-	} catch (e) {
-		throw new RuleOfOneException("CHARACTER");
-	}
-	
-	try {
-		opponentSuccesses = successTest(valueOfOpponent, valueOfCharacter);
-	} catch (e) {
-		throw new RuleOfOneException("OPPONENT");
-	}
-
-	return characterSucesses - opponentSuccesses;
-	
-}
-
-function RuleOfOneException (party) {
-	this.party = party;
+	return result;
 }
 
 function GreaterOrEqualTargetNumberFilter (targetNumber) {
+	
+	var tn = targetNumber;
+	
 	this.apply = function (x) {
-		return x >= targetNumber;
+		return x >= tn;
 	}	
 }
 
-/***************************
- * Prototyping
- ***************************/
-
-Array.max = function( array ) {
-    return Math.max.apply( Math, array );
-};
-
-
-/***************************
- * Classes
- ***************************/
-
-function Multiset() {
-
-	var collection = new Object();
-
-	this.add = function (key) {
-		if (!this.exists(key)) {
-			collection[key] = 1;
-		} else {
-			collection[key] = collection[key] + 1;
-		}
+/**
+ * Returns how often an element occurs in an array
+ */
+function count(myArr, el) {
+	var b = {};
+	var i = myArr.length;
+	var j;
+	while( i-- ) {
+		j = b[myArr[i]];
+		b[myArr[i]] = j ? j+1 : 1;
 	}
-
-	this.exists = function (key) {
-		return collection[key] != null;
-	}	
-
-	this.toString = function () {
-		var string = "";
-		for (property in collection) {
-			if (collection.hasOwnProperty(property)) {
-				string += collection[property] + "x " + property + ", ";
-			}
-		}	
-		return string;
-	}
-
-	this.count = function (element) {
-		if (this.exists(element)) {
-			return collection[element];
-		}
-		return 0;
-	}
-
-	this.setCount = function (element, count) {
-			collection[element] = count;
-	}
-
-	this.removeElement = function (element) {
-		if (this.exists(element)) {
-			delete collection[element];
-		}
-	}
-
-	this.collection = function () {
-		return collection;
-	}
-
-	this.addAll = function (multiset) {
-		var c = multiset.collection();
-		for (property in c) {
-			if (c.hasOwnProperty(property)) {
-				this.setCount(property, parseInt(multiset.count(property)) + parseInt(this.count(property)));
-			}
-		}
-	}
-
-	this.size = function () {
-		var size = 0;
-		for (property in collection) {
-			if (collection.hasOwnProperty(property)) {
-				size += this.count(property);
-			}
-		}
-		return size;
-	}
-
-	this.asArray = function () {
-		var myArray = [];
-
-		for (property in collection) {
-			if (collection.hasOwnProperty(property)) {
-				var count = this.count(property);
-				for (var i = 0; i < count; i++) {
-					myArray.push(property);
-				}
-			}
-		}
-		return myArray;
-	}
+	return b[el];
 }
+
 
 function DiceCup (numberOfSides) {
 	
-	this.roll = function (numberOfDice) {
-		var s = new Multiset();
-		var roll;
+	this.rollOnce = function (numberOfDice) {
+		var cup = new Array();
+		var dice;
 		for (i=0; i < numberOfDice; i++) {
-			roll = Math.round(Math.random() * numberOfSides) % numberOfSides + 1;
-			s.add(roll);
+			dice = Math.round(Math.random() * numberOfSides) % numberOfSides + 1;
+			cup.push(dice);
 		}
-		return s;
+		return cup;
 	}
 	
-	this.cumulativeRoll = function (numberOfDice, rerollAt) {
-		var c = this.roll(numberOfDice);
-		var cumulativeRerollAt = rerollAt;
-		var numberOfDiceToReroll;
-		while (c.count(cumulativeRerollAt) > 0) {
-			numberOfDiceToReroll = c.count(cumulativeRerollAt);
-			c.removeElement(cumulativeRerollAt);
-			var reroll = this.roll(numberOfDiceToReroll);
-			c.addAll(this.transform (reroll, cumulativeRerollAt));
-			cumulativeRerollAt += rerollAt;
-		}
-		return c;
+	this.roll = function (numberOfDice, rerollAt) {
+		var a = this.rollOnce(numberOfDice);
+		return this.reroll (a, rerollAt).sort(function(a,b){return a - b});
 	}
 
-	this.transform = function (set, cumulativeRerollAt) {
-		var c = set.collection();
-		for (property in c) {
-			if (c.hasOwnProperty(property)) {
-				set.setCount(parseInt(property) + parseInt(cumulativeRerollAt), c[property]);
-				set.removeElement(property);
+	this.reroll = function (originalArray, rerollAt) {
+		var j = 0;
+		while (j < originalArray.length) {
+			if (originalArray[j] % rerollAt == 0) {
+					originalArray[j] += this.rollOnce(1)[0];
+			} else {
+				j++;
 			}
 		}
-		return set;
+		return originalArray;
 	}
 }
-
-
-
-
-
-
-
-
-
-
